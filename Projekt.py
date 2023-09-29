a8e20983b151c95fd72859ed90800ea0cca5094b
akosfodor <144988738+akosfodor@users.noreply.github.com>
2023-09-29 14:17:22 +0200

    IKT projekt Rendezés.py
    Teljesen kész!

--- Projekt.py
+++ Projekt.py
@@ -1,22 +1,132 @@
-szamok=["1","2","3","4","5","6","7","8","9","0"]
-betuk=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a", "e", "i", "o", "u", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "z"]
+szamok="0123456789"
+betuk="ABCDEFGHIJKLMNOPQRSTUVWXYZaeioubcdfghjklmnprstvz"
 adatok=[]
 with open("ki.txt", "r", encoding="utf-8") as file:
-    for sor in file:
-        adatok.append(sor)
+    adatok = file.read().split(";")
+del adatok[-1]
 
-for x in szamok:
-    if x in adatok:
-        kerdes=input("Növekvő vagy csökkenő rendezés? (növekvő/csökkenő) ")
-        if kerdes=="növekvő":
-            adatok.sort(reverse=False, key=None)
-            print(adatok)
-        elif kerdes=="csökkenő":
-            adatok.sort(reverse=True, key=None)
-            print(adatok)
-        
-        else:
-            print("Hibás bevitel.")
+if not all(x in szamok + betuk or ';' for x in adatok):
+    print("Helytelen adatok.")
+else:
+    kerdes = input("Növekvő vagy csökkenő rendezés? (növekvő/csökkenő) ").lower()
+    print("Buborékrendezés:")
+    n = len(adatok)
 
-for x in betuk:
-    if x in adatok:
+    if kerdes == "növekvő":
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if adatok[j] > adatok[j+1]:
+                    adatok[j], adatok[j+1] = adatok[j+1], adatok[j]
+        if(x in szamok for x in adatok):
+            print("Növekvő sorrendben rendezett számok: " + '; '.join(adatok))
+        elif(x in betuk for x in adatok):
+            print("Növekvő sorrendben rendezett betűk: " + '; '.join(adatok))
+    elif kerdes == "csökkenő":
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if adatok[j] < adatok[j+1]:
+                    adatok[j], adatok[j+1] = adatok[j+1], adatok[j]
+        if(x in szamok for x in adatok):
+            print("Növekvő sorrendben rendezett számok: " + '; '.join(adatok))
+        elif(x in betuk for x in adatok):
+            print("Növekvő sorrendben rendezett betűk: " + '; '.join(adatok))
+    else:
+        print("Hibás bevitel.")
+
+    print("Összefésüléses rendezés: ")
+
+    if kerdes == "növekvő":
+        n = len(adatok)
+        if n > 1:
+            middle = n // 2
+            left_half = adatok[:middle]
+            right_half = adatok[middle:]
+
+            left_half.sort()
+            right_half.sort()
+
+            i, j, k = 0, 0, 0
+
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] < right_half[j]:
+                    adatok[k] = left_half[i]
+                    i += 1
+                else:
+                    adatok[k] = right_half[j]
+                    j += 1
+                k += 1
+
+            while i < len(left_half):
+                adatok[k] = left_half[i]
+                i += 1
+                k += 1
+
+            while j < len(right_half):
+                adatok[k] = right_half[j]
+                j += 1
+                k += 1
+        if(x in szamok for x in adatok):
+            print("Növekvő sorrendben rendezett számok: " + '; '.join(adatok))
+        elif(x in betuk for x in adatok):
+            print("Növekvő sorrendben rendezett betűk: " + '; '.join(adatok))
+    elif kerdes == "csökkenő":
+        n = len(adatok)
+        if n > 1:
+            middle = n // 2
+            left_half = adatok[:middle]
+            right_half = adatok[middle:]
+
+            left_half.sort(reverse=True)
+            right_half.sort(reverse=True)
+
+            i, j, k = 0, 0, 0
+
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] > right_half[j]:
+                    adatok[k] = left_half[i]
+                    i += 1
+                else:
+                    adatok[k] = right_half[j]
+                    j += 1
+                k += 1
+
+            while i < len(left_half):
+                adatok[k] = left_half[i]
+                i += 1
+                k += 1
+
+            while j < len(right_half):
+                adatok[k] = right_half[j]
+                j += 1
+                k += 1
+        if(x in szamok for x in adatok):
+            print("Növekvő sorrendben rendezett számok: " + '; '.join(adatok))
+        elif(x in betuk for x in adatok):
+            print("Növekvő sorrendben rendezett betűk: " + '; '.join(adatok))
+    else:
+        print("Hibás bevitel.")
+
+    while True:
+        uj_elem = input("Adjon meg egy új számot, vagy nyomjon egy entert a kilépéshez: ")
+
+        if not uj_elem:  
+            break
+
+        if kerdes == "növekvő":
+            for i, elem in enumerate(adatok):
+                if uj_elem < elem:
+                    adatok.insert(i, uj_elem)
+                    break
+            else:
+                adatok.append(uj_elem)
+                break
+        elif kerdes == "csökkenő":
+            for i, elem in enumerate(adatok):
+                if uj_elem > elem:
+                    adatok.insert(i, uj_elem)
+                    break
+            else:
+                adatok.append(uj_elem)
+                break
+
+    print("Az új rendezett lista: " + '; '.join(adatok))
